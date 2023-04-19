3ec43c7d4adfcfe110a6d0c8d695ab6b20f4999b
LexanderO <alexanderowens1993@gmail.com>
2023-04-19 11:18:54 +0100

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login></Login>,
+  },
+  {
+    path: "/register",
+    element: <Register></Register>,
+  },
+  {
+    path: "/login",
+    element: <Login></Login>,
+  },
+  {
+    path: "*",
+    element: <PageNotFound></PageNotFound>,
+  },
+]);
+
 const App = () => {
   return (
-    <div>
-      <p>Hello</p>
-    </div>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 };
 
