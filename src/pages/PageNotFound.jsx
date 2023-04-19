4a77558a9d9337f18ba66d4569c3fc46bf3f384f
LexanderO <alexanderowens1993@gmail.com>
2023-04-19 13:30:07 +0100

    updated page not found

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 
+import Logo from "../assets/polyglotter10x.png";
+
 const PageNotFound = () => {
   return (
-    <div>
-      <h3>Error 404 - Page not Found</h3>
-    </div>
+    <section className="h-screen">
+      <div className="pt-12 flex flex-wrap justify-center items-center">
+        <div className="sm:w-1/2">
+          <div className="px-5">
+            <img src={Logo} className="min-w-[60%]" alt="Polyglotter Logo" />
+          </div>
+        </div>
+        <div className="sm:w-1/4 lg:w-1/3 w-1/2">
+          <h2>Error 404, Can't seem to find the page </h2>
+        </div>
+      </div>
+    </section>
   );
 };
 
